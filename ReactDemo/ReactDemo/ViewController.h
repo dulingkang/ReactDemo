0d0139b8298e97fdc5f86256652486b9902c634c
dulingkang@163.com <dulingkang@163.com>
2016-06-24 14:35:58 +0800

    原生iOS，引入React

---
+++ ReactDemo/ReactDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReactDemo
+//
+//  Created by ShawnDu on 16/6/24.
+//  Copyright © 2016年 ShawnDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
--- ReactTest/ios/Pods/Target Support Files/Pods-ReactTestTests/Pods-ReactTestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactTestTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactTestTests
-@end
